75847dd3a013958a59de89377d4ab725dad48bc3
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-21 20:57:47 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -7,14 +7,14 @@ import sys
 from os import path
 
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		sys.stderr.write(f'Usage: {sys.argv[0]} README.md README.html\n')
-		exit(1)
-	else:
-		markDownFile = sys.argv[1]
-		outputFile = sys.argv[2]
-		if not path.exists(markDownFile):
-			sys.stderr.write(f'Missing {markDownFile}\n')
-			exit(1)
-		else:
-			exit(0)
+    if len(sys.argv) < 3:
+        sys.stderr.write(f'Usage: {sys.argv[0]} README.md README.html\n')
+        exit(1)
+    else:
+        markDownFile = sys.argv[1]
+        outputFile = sys.argv[2]
+        if not path.exists(markDownFile):
+            sys.stderr.write(f'Missing {markDownFile}\n')
+            exit(1)
+        else:
+            exit(0)
