85d9418ae0a893baf8eb660571ebab7b223acc81
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-04-01 19:26:28 +0100

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,24 @@ import sys
 from os import path
 
 if __name__ == "__main__":
+
+    def readHashTag(line, target="#"):
+        """
+        counts the amount of times '#' occurs in a line
+        Args:
+                line (str): the particular line to be read
+                target (str): the targeted symbol
+        Return:
+                hashCount (int): amount of hashtag found
+        """
+        hashCount = 0
+        for char in line:
+            if char == target:
+                hashCount += 1
+            else:
+                break
+        return hashCount
+
     if len(sys.argv) < 3:
         sys.stderr.write(f'Usage: {sys.argv[0]} README.md README.html\n')
         exit(1)
@@ -16,5 +34,12 @@ if __name__ == "__main__":
         if not path.exists(markDownFile):
             sys.stderr.write(f'Missing {markDownFile}\n')
             exit(1)
-        else:
-            exit(0)
+
+        with open(markDownFile, 'r') as md, open(outputFile, 'w') as html:
+            for readLine in md:
+                count = readHashTag(readLine)
+                if count > 0:
+                    html.write(f"<h{count}>{readLine.strip()[count:].strip()}</h{count}>\n")
+                else:
+                    html.write(readLine)
+        exit(0)
